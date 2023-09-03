bc8f5ad3b4b00f071f2aacfc479fbee7fb4613e4
beerappabvgp <beerappabharathb@gmail.com>
2023-09-03 00:44:13 -0700

    Added login functionality by using user_login() view function

--- quiz/urls.py
+++ quiz/urls.py
@@ -6,4 +6,5 @@ app_name = 'quiz'
 urlpatterns = [
     path('',views.home , name = "home"),
     path('register/',views.register , name = 'register'),
+    path('login/',views.user_login , name='login'),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render
 from .forms import CustomUsercreationForm
-from django.contrib.auth import login
+from django.contrib.auth import login,authenticate
 from django.contrib import messages
 
 
@@ -21,3 +21,18 @@ def register(request):
         form = CustomUsercreationForm()
 
     return render(request,'quiz/register.html' , {'form' : form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request,username = username , password = password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('quiz:home')
+        else:
+            messages.error(request,'Invalid username or password . Please try again .')
+
+    return render(request,'quiz/login.html')
